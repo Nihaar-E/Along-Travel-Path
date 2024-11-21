47f240c7f4bef68d2ab7d90ef983d15341340376
Nihaar-E <edulanihaarreddy16@gmail.com>
2024-11-21 22:02:03 +0530

    final commit

--- src/commonUtils/Constants.js
+++ src/commonUtils/Constants.js
@@ -1,6 +1,7 @@
-
-export const GOOGLE_MAPS_API_KEY = "AIzaSyBB6QR9xfg19VREe2nswPgWiEQRy4h-wh0" ;
-export const OPEN_WEATHER_MAP_API_KEY = "54e5542f2270719809f3ce03f04e5b61" ;
+import dotenv from "dotenv"
+dotenv.config()
+export const GOOGLE_MAPS_API_KEY =process.env.GOOGLE_MAPS_API_KEY  ;
+export const OPEN_WEATHER_MAP_API_KEY =process.env.OPEN_WEATHER_MAP_API_KEY  ;
 
 // This is to load google maps in the app...
 export const GOOGLE_MAPS_JS = "https://maps.googleapis.com/maps/api/js";
