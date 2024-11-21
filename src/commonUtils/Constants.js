7847a72298382fcb0465f1e3117ac9082505dcbf
Nihaar-E <edulanihaarreddy16@gmail.com>
2024-11-21 21:01:11 +0530

    initial commit

--- src/commonUtils/Constants.js
+++ src/commonUtils/Constants.js
@@ -1,7 +1,7 @@
-export const GOOGLE_MAPS_API_KEY = "AIzaSyBB6QR9xfg19VREe2nswPgWiEQRy4h-wh0";
-export const OPEN_WEATHER_MAP_API_KEY = "54e5542f2270719809f3ce03f04e5b61";
 
-// 03404a165ee79c56bc998717b0ed990c
+export const GOOGLE_MAPS_API_KEY = "AIzaSyBB6QR9xfg19VREe2nswPgWiEQRy4h-wh0" ;
+export const OPEN_WEATHER_MAP_API_KEY = "54e5542f2270719809f3ce03f04e5b61" ;
+
 // This is to load google maps in the app...
 export const GOOGLE_MAPS_JS = "https://maps.googleapis.com/maps/api/js";
 
